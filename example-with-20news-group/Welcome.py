cf4ff7b77b086ae4c51a6745e8e6b5390b43cad4
Giliard Godoi <giliard.godoi@gmail.com>
2023-08-11 22:06:50 -0300

    fix minor errors

--- example-with-20news-group/Welcome.py
+++ example-with-20news-group/Welcome.py
@@ -5,19 +5,20 @@ import spacy
 import spacy_streamlit
 import streamlit as st
 
+from spacy_streamlit import load_model
 from spacy_streamlit import visualize_ner
 
-st.cache_resource
-def load_model(model : str = 'en_core_web_sm'):
-    nlp = None
-    try:
-        nlp = spacy.load(model)
-    except ModuleNotFoundError:
-        print(f"downloading model {model} ...")
-        spacy.cli.download(model)
-        nlp = spacy.load(model)
+# st.cache_resource
+# def load_model(model : str = 'en_core_web_sm'):
+#     nlp = None
+#     try:
+#         nlp = spacy.load(model)
+#     except IOError:
+#         print(f"downloading model {model} ...")
+#         spacy.cli.download(model)
+#         nlp = spacy.load(model)
     
-    return nlp
+#     return nlp
 
 st.cache_data
 def load_text_data(dataset_name : str):
@@ -27,7 +28,7 @@ def load_text_data(dataset_name : str):
 
 
 def main():
-    nlp = load_model()
+    nlp = load_model('en-core-web-sm')
     data = load_text_data()
 
     st.title("Named Entity Recognition example")
@@ -45,6 +46,9 @@ def main():
 
     with default_visualizer:
         visualize_ner(doc, labels=nlp.get_pipe("ner").labels, show_table=False)
+
+    with custom_visualizer:
+        st.write("Not yet implemented!")
 
 
 
