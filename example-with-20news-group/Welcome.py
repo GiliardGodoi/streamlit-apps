9181e8a486c5d21cf0c889ddd4bdaad42e2e0c81
Giliard Godoi <giliard.godoi@gmail.com>
2023-08-12 00:28:09 -0300

    Will it work?

--- example-with-20news-group/Welcome.py
+++ example-with-20news-group/Welcome.py
@@ -9,28 +9,12 @@ from sklearn.datasets import fetch_20newsgroups
 from spacy_streamlit import load_model
 from spacy_streamlit import visualize_ner
 
-# st.cache_resource
-# def load_model(model : str = 'en_core_web_sm'):
-#     nlp = None
-#     try:
-#         nlp = spacy.load(model)
-#     except IOError:
-#         print(f"downloading model {model} ...")
-#         spacy.cli.download(model)
-#         nlp = spacy.load(model)
-    
-#     return nlp
-
 def load_text_data():
-    # it did not work well
-    # result = fetch_20newsgroups()
     text = None
     with open('70.txt', 'r', encoding='utf8') as f:
         text = f.read()
 
     return text
-
-
 
 def main():
     st.title("Named Entity Recognition Example")
